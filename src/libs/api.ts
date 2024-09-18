2ce602a791f53e1da7b695575122418eb7f04861
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-18 00:19:47 -0500

    feat: add necessary libs

---
+++ src/libs/api.ts
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+/* ➡ Configurando axios para las peticiones a la API */
+export default axios.create({
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://smart-comanda.vercel.app/api'
+})
