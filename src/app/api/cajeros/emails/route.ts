365034157814b46045622c3cd881b8e3588aabff
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-30 15:54:26 -0500

    fix: change some details

--- src/app/api/cajeros/emails/route.ts
+++ src/app/api/cajeros/emails/route.ts
@@ -1,18 +1,18 @@
-import { db } from '@/libs/prismaDB'
-import { NextResponse } from 'next/server'
+// import { db } from '@/libs/prismaDB'
+// import { NextResponse } from 'next/server'
 
 export async function GET () {
-  try {
-    const cajeros = await db.cajeros.findMany()
-    const emailsCajeros = cajeros.map((cajero) => cajero.correo)
+  // try {
+  //   const cajeros = await db.cajeros.findMany()
+  //   const emailsCajeros = cajeros.map((cajero) => cajero.correo)
 
-    return NextResponse.json({ emailsCajeros })
-  } catch (error) {
-    console.error({ error })
+  //   return NextResponse.json({ emailsCajeros })
+  // } catch (error) {
+  //   console.error({ error })
 
-    return NextResponse.json(
-      { message: 'Something went wrong.', error },
-      { status: 500 }
-    )
-  }
+  //   return NextResponse.json(
+  //     { message: 'Something went wrong.', error },
+  //     { status: 500 }
+  //   )
+  // }
 }
