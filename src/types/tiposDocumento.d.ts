86d462e0c7cf8dd6ec13dd62971c988c382ae75c
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-09-29 22:54:44 -0500

    feat: add new types

---
+++ src/types/tiposDocumento.d.ts
@@ -0,0 +1,7 @@
+/* ➡ Interfaz con los tipos de las propiedades de las Tipos de Documento */
+export interface TipoDocumento {
+  id_tipo_documento: string
+  tipo_documento: string
+  createdAt: Date
+  updatedAt: Date
+}
