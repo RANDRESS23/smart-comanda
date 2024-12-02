30be6fd409b529d256e3a0541c28f967074ef282
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-12-02 11:22:35 -0500

    feat: add new hook

---
+++ src/hooks/useComandas.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import api from '@/libs/api'
+import { type Comanda } from '@/types/comandas'
+import { format } from '@formkit/tempo'
+
+/* ➡ Hook para manejar las comandas del restaurante */
+export const useComandas = () => {
+  const [comandas, setComandas] = useState<Comanda[]>([])
+  const [loadingComandas, setLoadingComandas] = useState(false)
+
+  const getComandas = async () => {
+    try {
+      setLoadingComandas(true)
+
+      const fecha = format(new Date(), 'YYYY-MM-DD')
+      const response = await api.get(`/comanda/${fecha}`)
+
+      const idsComandasPrev = comandas.map(({ id_comanda: idComanda }) => idComanda)
+      const idsComandasPost = (response?.data?.comandas as Comanda[]).map(({ id_comanda: idComanda }) => idComanda)
+
+      if (idsComandasPrev.toString() !== idsComandasPost.toString()) {
+        setComandas(response?.data?.comandas as Comanda[])
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoadingComandas(false)
+    }
+  }
+
+  useEffect(() => {
+    getComandas()
+  }, [])
+
+  return { comandas, getComandas, setComandas, loadingComandas }
+}
